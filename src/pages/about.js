a99788b011851ce865faae38456afc2a9bbfec7d
Nthuyduong <nguyenthuyduong15113@gmail.com>
2023-11-08 09:15:24 +0700

    about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,15 +4,15 @@ const About = () => {
     return (
         <div className="about-blog-page container-fluid">
             <div className="about-banner">
-                <img className="w-100" src="./img/about/banner.jpg" alt="smile" loading="lazy"/>
+                <img className="w-full" src="./img/about/banner.jpg" alt="smile" loading="lazy"/>
             </div>
             <div className="about-intro">
                 <div className="intro-decor absolute">
-                    <img className="w-100" src="./img/about/decor.svg" alt="smile" loading="lazy"/>
+                    <img className="w-full" src="./img/about/decor.svg" alt="smile" loading="lazy"/>
                 </div>
-                <div className="intro-inner grid grid-cols-4 gap-4 z-30">
+                <div className="intro-inner grid grid-cols-4 gap-4 ">
                     <div className=""></div>
-                    <div className="col-span-2 intro-content heading_5 text-center">
+                    <div className="col-span-2 intro-content heading_5 text-center z-30">
                         “Tempus, tristique morbi scelerisque sed. Diam nec ut sed est sit in tortor. Blandit consequat quisque
                         vitae ornare diam netus tellus. Tempus, tristique morbi scelerisque sed. Diam nec ut sed est sit in tortor.
                         Blandit consequat quisque.”
@@ -23,7 +23,7 @@ const About = () => {
             <div className="about-me p-60">
                 <div className="grid grid-cols-12 gap-5">
                     <div className="col-span-5">
-                        <img className="w-100" src="./img/about/abt-me.jpg" alt="smile" loading="lazy"/>
+                        <img className="w-full" src="./img/about/abt-me.jpg" alt="smile" loading="lazy"/>
                     </div>
                     <div className="col-span-7 about-right flex">
                         <div className="about-content m-auto">
@@ -43,8 +43,11 @@ const About = () => {
             </div>
             <div className="what-we-do">
                 <div className="grid grid-cols-12 gap-5">
-                    <div className="col-span-3">
-                        <div className="heading_5 mb-2">
+                    <div className="col-span-3 we-do">
+                        <div>
+                            <img className="w-100" src="./img/about/do.svg" alt="smile" loading="lazy"/>
+                        </div>
+                        <div className="heading_5 my-2">
                             Making My Passion
                         </div>
                         <div>
@@ -52,8 +55,11 @@ const About = () => {
                             vitae ornare diam netus tellus. Tempus, tristique morbi scelerisque sed. Diam nec ut sed est sit in.
                         </div>
                     </div>
-                    <div className="col-span-3">
-                        <div className="heading_5 mb-2">
+                    <div className="col-span-3 we-do">
+                        <div>
+                            <img className="w-100" src="./img/about/do.svg" alt="smile" loading="lazy"/>
+                        </div>
+                        <div className="heading_5 my-2">
                             Explore My Hometown
                         </div>
                         <div>
@@ -61,8 +67,11 @@ const About = () => {
                             vitae ornare diam netus tellus. Tempus, tristique morbi scelerisque sed. Diam nec ut sed est sit in.
                         </div>
                     </div>
-                    <div className="col-span-3">
-                        <div className="heading_5 mb-2">
+                    <div className="col-span-3 we-do">
+                        <div>
+                            <img className="w-100" src="./img/about/do.svg" alt="smile" loading="lazy"/>
+                        </div>
+                        <div className="heading_5 my-2">
                             Sharing All Destinations
                         </div>
                         <div>
@@ -70,8 +79,11 @@ const About = () => {
                             vitae ornare diam netus tellus. Tempus, tristique morbi scelerisque sed. Diam nec ut sed est sit in.
                         </div>
                     </div>
-                    <div className="col-span-3">
-                        <div className="heading_5 mb-2">
+                    <div className="col-span-3 we-do">
+                        <div>
+                            <img className="w-100" src="./img/about/do.svg" alt="smile" loading="lazy"/>
+                        </div>
+                        <div className="heading_5 my-2">
                             Connect With You
                         </div>
                         <div>
@@ -83,7 +95,7 @@ const About = () => {
             </div>
             <div className="about-blog">
                 <div className="grid grid-cols-12 about-blog-inner flex">
-                    <div className="col-span-6 about-content m-auto">
+                    <div className="col-span-6 about-content m-auto p-6">
                         <div className="heading_3 mb-2">About blog</div>
                         <div className="">
                             Tempus, tristique morbi scelerisque sed. Diam nec ut sed est sit in tortor. Blandit consequat
@@ -102,7 +114,7 @@ const About = () => {
                     <div className="col-span-6 pr-5 img-mission">
                         <img className="w-100" src="./img/about/mission.jpg" alt="smile" loading="lazy"/>
                     </div>
-                    <div className="col-span-6 mission-content m-auto">
+                    <div className="col-span-6 mission-content m-auto p-6">
                         <div className="heading_3 mb-2">My mission</div>
                         <div>
                             Tempus, tristique morbi scelerisque sed. Diam nec ut sed est sit in tortor. Blandit consequat
