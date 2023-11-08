e96648251c8a5a14c0ed858db0efc56bb8f3e79f
Nthuyduong <nguyenthuyduong15113@gmail.com>
2023-11-08 10:11:20 +0700

    commit

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,152 @@ import React from "react";
 
 const Home = () => {
     return (
-        <div></div>
+        <div>
+            <div className="container-fluid">
+                {/*Lastest on this blog*/}
+                <div className="recently-update">
+                    <div className="heading_3 mb-4">Recently Update</div>
+                    <div className="grid grid-cols-12 gap-4 recently-update-blog">
+                        <div className="col-span-4">
+                            <div>
+                                <img className="w-100" src="./img/home/article.jpg" alt="smile" loading="lazy"/>
+                            </div>
+                            <div>
+                                <div className="article-info py-1 mb-1">
+                                    <div className="flex mb-1">
+                                        <div className="mr-auto">Drink & Coffee</div>
+                                        <div>November 8, 2023</div>
+                                    </div>
+                                    <div className="heading_5">Top 5 beautiful Coffee Shop in HaNoi</div>
+                                </div>
+                                <div className="flex">
+                                    <div className="mr-auto">Read more</div>
+                                    <div>Share</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-span-4">
+                            <div>
+                                <img className="w-100" src="./img/home/article.jpg" alt="smile" loading="lazy"/>
+                            </div>
+                            <div>
+                                <div className="article-info py-1 mb-1">
+                                    <div className="flex mb-1">
+                                        <div className="mr-auto">Drink & Coffee</div>
+                                        <div>November 8, 2023</div>
+                                    </div>
+                                    <div className="heading_5">Top 5 beautiful Coffee Shop in HaNoi</div>
+                                </div>
+                                <div className="flex">
+                                    <div className="mr-auto">Read more</div>
+                                    <div>Share</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-span-4">
+                            <div>
+                                <img className="w-100" src="./img/home/article.jpg" alt="smile" loading="lazy"/>
+                            </div>
+                            <div>
+                                <div className="article-info py-1 mb-1">
+                                    <div className="flex mb-1">
+                                        <div className="mr-auto">Drink & Coffee</div>
+                                        <div>November 8, 2023</div>
+                                    </div>
+                                    <div className="heading_5">Top 5 beautiful Coffee Shop in HaNoi</div>
+                                </div>
+                                <div className="flex">
+                                    <div className="mr-auto">Read more</div>
+                                    <div>Share</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="p-60"></div>
+                    <div className="grid grid-cols-4">
+                        <div className="col-span-2">a</div>
+                        <div className="col-span-2">a</div>
+                    </div>
+                <div>
+                    <div className="heading_3 mb-4">Reader's Favorite</div>
+                    <div className="grid grid-cols-12 gap-4 recently-update-blog">
+                        <div className="col-span-3">
+                            <div>
+                                <img className="w-100" src="./img/home/article.jpg" alt="smile" loading="lazy"/>
+                            </div>
+                            <div>
+                                <div className="article-info py-1 mb-1">
+                                    <div className="flex mb-1">
+                                        <div className="mr-auto">Drink & Coffee</div>
+                                        <div>November 8, 2023</div>
+                                    </div>
+                                    <div className="heading_5">Top 5 beautiful Coffee Shop in HaNoi</div>
+                                </div>
+                                <div className="flex">
+                                    <div className="mr-auto">Read more</div>
+                                    <div>Share</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-span-3">
+                            <div>
+                                <img className="w-100" src="./img/home/article.jpg" alt="smile" loading="lazy"/>
+                            </div>
+                            <div>
+                                <div className="article-info py-1 mb-1">
+                                    <div className="flex mb-1">
+                                        <div className="mr-auto">Drink & Coffee</div>
+                                        <div>November 8, 2023</div>
+                                    </div>
+                                    <div className="heading_5">Top 5 beautiful Coffee Shop in HaNoi</div>
+                                </div>
+                                <div className="flex">
+                                    <div className="mr-auto">Read more</div>
+                                    <div>Share</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-span-3">
+                            <div>
+                                <img className="w-100" src="./img/home/article.jpg" alt="smile" loading="lazy"/>
+                            </div>
+                            <div>
+                                <div className="article-info py-1 mb-1">
+                                    <div className="flex mb-1">
+                                        <div className="mr-auto">Drink & Coffee</div>
+                                        <div>November 8, 2023</div>
+                                    </div>
+                                    <div className="heading_5">Top 5 beautiful Coffee Shop in HaNoi</div>
+                                </div>
+                                <div className="flex">
+                                    <div className="mr-auto">Read more</div>
+                                    <div>Share</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-span-3">
+                            <div>
+                                <img className="w-100" src="./img/home/article.jpg" alt="smile" loading="lazy"/>
+                            </div>
+                            <div>
+                                <div className="article-info py-1 mb-1">
+                                    <div className="flex mb-1">
+                                        <div className="mr-auto">Drink & Coffee</div>
+                                        <div>November 8, 2023</div>
+                                    </div>
+                                    <div className="heading_5">Top 5 beautiful Coffee Shop in HaNoi</div>
+                                </div>
+                                <div className="flex">
+                                    <div className="mr-auto">Read more</div>
+                                    <div>Share</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
