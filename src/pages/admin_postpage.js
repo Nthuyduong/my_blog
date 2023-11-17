1f6bc45b3e014681efeaad79cd3fff6605071d9f
Nthuyduong <nguyenthuyduong15113@gmail.com>
2023-11-17 20:21:48 +0700

    commit

--- src/pages/admin_postpage.js
+++ src/pages/admin_postpage.js
@@ -2,67 +2,7 @@ import React from "react";
 
 const admin_postpage = () => {
     return(
-        <div>
-            {/*POST TABLE*/}
-            <div className="flex admin-tbl">
-                <div className="tbl-row admin-tbl-title">
-                    <div>ID</div>
-                    <div>Date & Time</div>
-                    <div>Feature image</div>
-                    <div>Post title</div>
-                    <div>Author</div>
-                    <div>Status</div>
-                    <div>Action</div>
-                </div>
-                <div className="post-list">
-                    <div className="flex">
-                        <div className="">ID</div>
-                        <div>Date & Time</div>
-                        <div>Feature image</div>
-                        <div>Post title</div>
-                        <div>Author</div>
-                        <div>Status</div>
-                        <div>Action</div>
-                    </div>
-                    <div className="flex">
-                        <div>ID</div>
-                        <div>Date & Time</div>
-                        <div>Feature image</div>
-                        <div>Post title</div>
-                        <div>Author</div>
-                        <div>Status</div>
-                        <div>Action</div>
-                    </div>
-                    <div className="flex">
-                        <div>ID</div>
-                        <div>Date & Time</div>
-                        <div>Feature image</div>
-                        <div>Post title</div>
-                        <div>Author</div>
-                        <div>Status</div>
-                        <div>Action</div>
-                    </div>
-                    <div className="flex">
-                        <div>ID</div>
-                        <div>Date & Time</div>
-                        <div>Feature image</div>
-                        <div>Post title</div>
-                        <div>Author</div>
-                        <div>Status</div>
-                        <div>Action</div>
-                    </div>
-                    <div className="flex">
-                        <div>ID</div>
-                        <div>Date & Time</div>
-                        <div>Feature image</div>
-                        <div>Post title</div>
-                        <div>Author</div>
-                        <div>Status</div>
-                        <div>Action</div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <div></div>
     )
 }
 export default admin_postpage;
--- src/pages/home.js
+++ src/pages/home.js
@@ -542,6 +542,78 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+            <div className="container-fluid">
+                {/*POST TABLE*/}
+                <div className="admin-tbl">
+                    <div className="flex tbl-row admin-tbl-title">
+                        <div className="cell-ssm">ID</div>
+                        <div className="cell">Date & Time</div>
+                        <div className="cell">Feature image</div>
+                        <div className="cell">Post title</div>
+                        <div className="cell">Author</div>
+                        <div className="cell">Status</div>
+                        <div className="cell">Action</div>
+                        <div className="cell">Action</div>
+                    </div>
+                    <div className="post-list">
+                        <div className="flex tbl-row">
+                            <div className="cell-ssm">ID</div>
+                            <div className="cell">Date & Time</div>
+                            <div className="cell">Feature image</div>
+                            <div className="cell">Post title</div>
+                            <div className="cell">Author</div>
+                            <div className="cell">Status</div>
+                            <div className="cell">
+                                <select className="sl-box">
+                                    <option value="" selected hidden>Choose status</option>
+                                    <option value="">Published</option>
+                                    <option value="">Draft</option>
+                                    <option value="">New</option>
+                                </select>
+                            </div>
+                            <div className="cell">Action</div>
+                        </div>
+                        <div className="flex tbl-row">
+                            <div className="cell-ssm">ID</div>
+                            <div className="cell">Date & Time</div>
+                            <div className="cell">Feature image</div>
+                            <div className="cell">Post title</div>
+                            <div className="cell">Author</div>
+                            <div className="cell">
+
+                            </div>
+                            <div className="cell">
+                                <select className="sl-box">
+                                    <option value="" selected hidden>Choose status</option>
+                                    <option value="">Published</option>
+                                    <option value="">Draft</option>
+                                    <option value="">New</option>
+                                </select>
+                            </div>
+                            <div className="cell">Action</div>
+                        </div>
+                        <div className="flex tbl-row">
+                            <div className="cell-ssm">ID</div>
+                            <div className="cell">Date & Time</div>
+                            <div className="cell">Feature image</div>
+                            <div className="cell">Post title</div>
+                            <div className="cell">Author</div>
+                            <div className="cell">
+
+                            </div>
+                            <div className="cell">
+                                <select className="sl-box">
+                                    <option value="" selected hidden>Choose status</option>
+                                    <option value="">Published</option>
+                                    <option value="">Draft</option>
+                                    <option value="">New</option>
+                                </select>
+                            </div>
+                            <div className="cell">Action</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
