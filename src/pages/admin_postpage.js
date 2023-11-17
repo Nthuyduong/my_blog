d67bbbecebd56e52610d3ff22e8864790c2c6207
Nthuyduong <nguyenthuyduong15113@gmail.com>
2023-11-17 20:29:43 +0700

    post paste

--- src/pages/admin_postpage.js
+++ src/pages/admin_postpage.js
@@ -2,7 +2,80 @@ import React from "react";
 
 const admin_postpage = () => {
     return(
-        <div></div>
+        <div>
+            <div className="container-fluid">
+                {/*POST TABLE*/}
+                <div className="admin-tbl">
+                    <div className="flex tbl-row admin-tbl-title">
+                        <div className="cell-ssm">ID</div>
+                        <div className="cell">Date & Time</div>
+                        <div className="cell">Feature image</div>
+                        <div className="cell">Post title</div>
+                        <div className="cell">Author</div>
+                        <div className="cell">Status</div>
+                        <div className="cell">Action</div>
+                        <div className="cell">Action</div>
+                    </div>
+                    <div className="post-list">
+                        <div className="flex tbl-row">
+                            <div className="cell-ssm">ID</div>
+                            <div className="cell">Date & Time</div>
+                            <div className="cell">Feature image</div>
+                            <div className="cell">Post title</div>
+                            <div className="cell">Author</div>
+                            <div className="cell">Status</div>
+                            <div className="cell">
+                                <select className="sl-box">
+                                    <option value="" selected hidden>Choose status</option>
+                                    <option value="">Published</option>
+                                    <option value="">Draft</option>
+                                    <option value="">New</option>
+                                </select>
+                            </div>
+                            <div className="cell">Action</div>
+                        </div>
+                        <div className="flex tbl-row">
+                            <div className="cell-ssm">ID</div>
+                            <div className="cell">Date & Time</div>
+                            <div className="cell">Feature image</div>
+                            <div className="cell">Post title</div>
+                            <div className="cell">Author</div>
+                            <div className="cell">
+
+                            </div>
+                            <div className="cell">
+                                <select className="sl-box">
+                                    <option value="" selected hidden>Choose status</option>
+                                    <option value="">Published</option>
+                                    <option value="">Draft</option>
+                                    <option value="">New</option>
+                                </select>
+                            </div>
+                            <div className="cell">Action</div>
+                        </div>
+                        <div className="flex tbl-row">
+                            <div className="cell-ssm">ID</div>
+                            <div className="cell">Date & Time</div>
+                            <div className="cell">Feature image</div>
+                            <div className="cell">Post title</div>
+                            <div className="cell">Author</div>
+                            <div className="cell">
+
+                            </div>
+                            <div className="cell">
+                                <select className="sl-box">
+                                    <option value="" selected hidden>Choose status</option>
+                                    <option value="">Published</option>
+                                    <option value="">Draft</option>
+                                    <option value="">New</option>
+                                </select>
+                            </div>
+                            <div className="cell">Action</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default admin_postpage;
